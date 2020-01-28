47d14c7e99d46c5f2dfa81505d0b816db062cab2
WSHuusfeldt <43413833+WSHuusfeldt@users.noreply.github.com>
2020-01-28 10:03:06 +0100

    changed name of method

--- src/main/java/Functionality/Game.java
+++ src/main/java/Functionality/Game.java
@@ -29,11 +29,12 @@ public class Game {
         
     }
     
-    public int checkRole(int val, int val2) {
+    public int getDiceValue(int val, int val2) {
         if (val > val2)
             return Integer.valueOf(String.valueOf(val) + String.valueOf(val2));
         return Integer.valueOf(String.valueOf(val2) + String.valueOf(val));
     } 
+    
     
     
 }
